101c5db88988cd1bd2a32378a7f9f5448c3b04a8
Garrett <Garrettcook115@gmail.com>
2026-04-02 19:10:00 -0400

    Added executable and zipped folder for this appplication.

--- Zip.py
+++ Zip.py
@@ -1,7 +1,7 @@
 from zipfile import ZipFile, ZIP_DEFLATED
 
-zip_path = './TkinterApp1.zip'
-file_to_zip = './TkinterApp1.py'
+zip_path = './TkinterApp1 Executable.zip'
+file_to_zip = './TkinterApp1 Executable.py'
 
 with ZipFile(zip_path, 'w', ZIP_DEFLATED )as zip:
     zip.write(file_to_zip) 
