0e8459b107ba7a15ab02f59befb497a45e215ef1
Garrett <Garrettcook115@gmail.com>
2026-04-02 19:17:50 -0400

    Updated README

--- Zip.py
+++
@@ -1,7 +0,0 @@
-from zipfile import ZipFile, ZIP_DEFLATED
-
-zip_path = './TkinterApp1 Executable.zip'
-file_to_zip = './TkinterApp1 Executable.py'
-
-with ZipFile(zip_path, 'w', ZIP_DEFLATED )as zip:
-    zip.write(file_to_zip) 
